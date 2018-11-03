b9d5a15d9a96c2fc55359fae9fec7f9c845dcac5
Sankalp Saxena <sankalp.saxena.sta@gmail.com>
2018-11-03 19:37:53 +0530

    Add files via upload

---
+++ src/jdbc/JDBC.java
@@ -0,0 +1,50 @@
+package jdbc;
+
+import java.sql.*;
+
+public class JDBC
+{
+    public static void main(String args[]) throws Exception
+    {
+      
+        String url="D";
+        String uname="root";
+        String pass="root";
+        
+        //start
+        long lStartTime = System.nanoTime();
+
+        
+        String query="select * from stud_details where stud_id = 2";	
+        try{
+        Class.forName("com.mysql.jdbc.Driver");
+        Connection con = DriverManager.getConnection(url, uname, pass);
+        Statement st = con.createStatement();
+        ResultSet rs = st.executeQuery(query);
+        
+        rs.next();
+        String name = rs.getString("stud_name");
+        System.out.println(name);
+        
+        //end
+        long lEndTime = System.nanoTime();
+
+  		//time elapsed
+        long output = lEndTime - lStartTime;
+
+        System.out.println("Elapsed time in milliseconds: " + output / 1000000);
+
+        
+        
+        st.close();
+        con.close();
+        }
+     
+        
+       
+        catch (Exception e)
+        {
+            System.out.println("Error: "+e);
+        }
+    }   
+}
